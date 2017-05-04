8b68488ea5b21edcf54214857b0ef52fff9c506c
Daryl Jacob <daryl@moustachemotion.com>
2017-05-04 16:46:08 -0400

    added tableview and working on second screen

--- Sleepy Bear/ViewController.swift
+++ Sleepy Bear/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var surgeryType: UITableView!
+    
+    var specialty = ["GYN", "OB", "UROGEN", "GEN", "NEURO", "ENT"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        surgeryType.dataSource = self
+        surgeryType.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return specialty.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = specialty[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
